f488cf17873c5b4598e87341d0dbaddfc3d85f47
Abel Koech <koechabel161@gmail.com>
2024-10-12 08:39:04 +0300

    review typos

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ class BirdByID(Resource):
     def get(self, id):
         bird = Bird.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(bird), 200)
-api.add_resource(BirdByID, 'birds/<int:id>')     
+api.add_resource(BirdByID, '/birds/<int:id>')     
